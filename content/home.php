88377bc3d4608a99137d8176a68505116cd2096a
Your Name <uedmasters@gmail.com>
2026-06-19 04:45:35 +0530

    feat(career): implement CareerRepository

--- content/home.php
+++ content/home.php
@@ -72,6 +72,7 @@ return [
         ['src' => '/public/img/logos/indigo.svg',       'alt' => 'IndiGo'],
         ['src' => '/public/img/logos/quikr.svg',        'alt' => 'Quikr'],
         ['src' => '/public/img/logos/intelegencia.svg', 'alt' => 'Intelegencia'],
+        ['src' => '/public/img/logos/indigo.svg',       'alt' => 'IndiGo'],
     ],
 
     /* ---- selected work: header copy (the LIST comes from projects.php) ---- */
